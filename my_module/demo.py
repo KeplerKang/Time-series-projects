5940ed19bf47def5c3b06f6b640612f9c0ffbe26
kjy <1779062080@qq.com>
2024-12-09 15:31:30 +0800

    init commit

---
+++ my_module/demo.py
@@ -0,0 +1,5 @@
+import numpy as np
+import matplotlib.pyplot as plt
+# loader = np.load('E:/TSF_proj/my_module_result/results/ETTm1_FEDformer_random_modes64_ETTm1_ftS_sl96_ll48_pl192_dm512_nh8_el2_dl1_df2048_fc3_ebtimeF_dtTrue_Exp_2/pred.npy')
+# print(loader)
+
